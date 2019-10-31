81a7b86f50eedf079ad490ff152c4e0056345754
gstieg <682@holberton.com>
2019-10-31 11:08:33 -0700

    Unity Portfolio Project
    Add All Files To Github

---
+++ New Portfolio/Assets/DieSpike.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DieSpike : MonoBehaviour
+{
+    BoxCollider2D myBodyCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBodyCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Die();
+    }
+    private void Die()
+    {
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Hazards")))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ New Portfolio/Assets/NewPlayer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewPlayer : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    Rigidbody2D myRigidBody;
+    BoxCollider2D myBodyCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myBodyCollider = GetComponent<BoxCollider2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        Jump();
+        Die();
+    }
+
+    private void Run()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            // Value is between -1 to +1
+            // create a new vector2 parameter value that only changes the x-axis velocity
+            Vector2 playerVelocity = new Vector2(runSpeed, myRigidBody.velocity.y);
+            // set the velocity field parameter values to be the new vector2 velocity values
+            myRigidBody.velocity = playerVelocity;
+            // bool that determines the animation for the player sprite when running
+            bool playerHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            // Value is between -1 to +1
+            // create a new vector2 parameter value that only changes the x-axis velocity
+            Vector2 playerVelocity = new Vector2(-runSpeed, myRigidBody.velocity.y);
+            // set the velocity field parameter values to be the new vector2 velocity values
+            myRigidBody.velocity = playerVelocity;
+            // bool that determines the animation for the player sprite when running
+            bool playerHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        }
+
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        // Alternate method: if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            // create a new vector2 parameter value that only changes the y-axis velocity
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            // add the new vector2 parameter values to the original velocity values
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+    private void Die()
+    {
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Hazard")))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+
+}
+
+        
